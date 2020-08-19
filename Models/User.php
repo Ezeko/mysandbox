3f0ada20f88ff825d4168abb735811fb615ed751
unknown <ezeko2017@gmail.com>
2020-08-19 11:58:39 +0100

    create insert and update method

---
+++ Models/User.php
@@ -0,0 +1,102 @@
+<?php
+namespace Models;
+
+
+class User{
+public $conn;
+ public function connectToDB(){
+   $db = new Database("localhost", "root", "", "wallet-funding");
+   return $db->connect();
+   
+ }
+
+  /**
+  * Gets all users details from database
+  * @param void
+  * @return $result
+  */
+ public function all(){
+    $sql = "SELECT * FROM `users` WHERE 1";
+    $conn = $this->connectToDB();
+    
+    $query = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+
+    if ($query) {
+       $result = [];
+       $check = mysqli_num_rows($query);
+       while ($check = mysqli_fetch_Assoc($query)) {
+         array_push($result, $check);
+       }
+       return $result;
+
+    } else {
+       return "Error ". mysqli_error($conn);
+    }
+ }
+
+ /**
+  * Gets user details from database
+  * @param array $where
+  * @return $result
+  */
+ public function get(array $where) {
+   $conn = $this->connectToDB();
+   
+   $sql = "SELECT * from `users` WHERE `$where[0]` $where[1] '$where[2]'"; ;
+
+   $query = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+
+    if ($query) {
+       $result = [];
+       $check = mysqli_num_rows($query);
+       while ($check = mysqli_fetch_Assoc($query)) {
+         array_push($result, $check);
+       }
+       return $result;
+
+    } else {
+       return "Error ". mysqli_error($conn);
+    }
+ }
+
+  /**
+  * Create a new user detail into the database
+  * @param array $input
+  * @return true
+  */
+  public function create (string $username, string $email) {
+     $conn = $this->connectToDB();
+
+     $sql = "INSERT INTO `users` (username, email) VALUES ('$username', '$email')";
+     $query = mysqli_query($conn, $sql);
+     if ($query){
+        return true;
+     }else{
+        die(mysqli_error($conn));
+        return false;
+     }
+  }
+
+    /**
+  * Create a new user detail into the database
+  * @param array $input
+  * @return true
+  */
+  public function update(array $details, array $whereClause) {
+     $conn = $this->connectToDB();
+
+     $sql = "UPDATE `users` SET `$details[0]` $details[1] $details[2]
+     WHERE `$whereClause[0]` $whereClause[1] '$whereClause[2]' ";
+
+     $query = mysqli_query($conn, $sql);
+
+     if ($query){
+      return true;
+   }else{
+      die(mysqli_error($conn));
+      return false;
+   }
+  }
+  
+
+}
--- Views/index.php
+++ Views/index.php
@@ -1,11 +1,21 @@
 <?php
 
-use Models\Credit;
+use Models\User;
 
 require ('../config.php');
 
-$db = new Credit;
+$db = new User;
+
+//print_r($db->create("ella", "ella@email.com"));
+
+$db->update(["amount", "=", 200], ["username", "=", "ezeko"]);
 
 print_r ($db->all());
- 
+
+print_r($user = ($db->get(['username', "!=", 'ezeko'])));
+echo $user[0]['amount'];
+ foreach ($user as $u){
+     echo $u['email'];
+ }
+
 ?>
